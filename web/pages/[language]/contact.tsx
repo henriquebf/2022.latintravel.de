6c9d8e50abde2e483765633bbef680a1f79e453c
Henrique Ferreira <henriquebf@gmail.com>
2022-01-29 14:06:13 +0100

    add content for contact section

--- web/pages/[language]/contact.tsx
+++ web/pages/[language]/contact.tsx
@@ -2,7 +2,9 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import { GetServerSideProps } from 'next';
 import Header from '@/components/sections/Header';
+import Content from '@/components/sections/Content';
 import Footer from '@/components/sections/Footer';
+import { getParagraphs } from '@/helpers/contentHelper';
 
 type Props = { language: string };
 
@@ -15,7 +17,16 @@ const Contact: NextPage<Props> = ({ language }) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header language={language} />
-      <main>Contact</main>
+      <main>
+        <Content
+          images={[]}
+          paragraphs={getParagraphs(language, [
+            'contact',
+            'content',
+            'paragraphs',
+          ])}
+        />
+      </main>
       <Footer />
     </div>
   );
