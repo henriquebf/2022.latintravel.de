b61a8f9c396ce18b2a148496b47a9ebe57c0bffc
Henrique Ferreira <henriquebf@gmail.com>
2022-01-30 17:12:32 +0100

    add localised content for footer and header

--- web/components/sections/Footer.tsx
+++ web/components/sections/Footer.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
 import Link from 'next/link';
 import Container from '@/components/layout/Container';
+import { getContent, getParagraphs } from '@/helpers/contentHelper';
 
 type Props = { language: string };
 
@@ -9,33 +10,178 @@ const About: NextPage<Props> = ({ language }) => {
     <footer>
       <Container>
         <nav>
-          <Link href={`/${language}`}>Home</Link>
-          <Link href={`/${language}/about`}>About</Link>
-          <Link href={`/${language}/contact`}>Contact</Link>
-          <Link href={`/${language}/data-protection`}>Data Protection</Link>
-          <Link href={`/${language}/disclaimer`}>Disclaimer</Link>
-          <Link href={`/${language}/impressum`}>Impressum</Link>
-          <Link href={`/${language}/services`}>Services</Link>
-          <Link href={`/${language}/event/business`}>business</Link>
-          <Link href={`/${language}/event/carnival`}>carnival</Link>
-          <Link href={`/${language}/experience/abrolhos`}>abrolhos</Link>
-          <Link href={`/${language}/experience/fernando-de-noronha`}>
-            fernando-de-noronha
-          </Link>
-          <Link href={`/${language}/experience/foz-do-iguacu`}>
-            foz-do-iguacu
-          </Link>
-          <Link href={`/${language}/experience/ilha-grande`}>ilha-grande</Link>
-          <Link href={`/${language}/experience/jericoacoara`}>
-            jericoacoara
-          </Link>
-          <Link href={`/${language}/experience/lencois`}>lencois</Link>
-          <Link href={`/${language}/experience/manaus`}>manaus</Link>
-          <Link href={`/${language}/experience/pantanal`}>pantanal</Link>
-          <Link href={`/${language}/experience/rio-de-janeiro`}>
-            rio-de-janeiro
-          </Link>
-          <Link href={`/${language}/experience/salvador`}>salvador</Link>
+          <div>
+            <h5>{getContent(language, ['global', 'experiences'])}</h5>
+            <ul>
+              <li>
+                <Link href={`/${language}/experience/lencois`}>
+                  {getContent(language, [
+                    'experiences',
+                    'lencois',
+                    'intro',
+                    'title',
+                  ])}
+                </Link>
+              </li>
+              <li>
+                <Link href={`/${language}/experience/ilha-grande`}>
+                  {getContent(language, [
+                    'experiences',
+                    'ilha-grande',
+                    'intro',
+                    'title',
+                  ])}
+                </Link>
+              </li>
+              <li>
+                <Link href={`/${language}/experience/foz-do-iguacu`}>
+                  {getContent(language, [
+                    'experiences',
+                    'foz-do-iguacu',
+                    'intro',
+                    'title',
+                  ])}
+                </Link>
+              </li>
+              <li>
+                <Link href={`/${language}/experience/fernando-de-noronha`}>
+                  {getContent(language, [
+                    'experiences',
+                    'fernando-de-noronha',
+                    'intro',
+                    'title',
+                  ])}
+                </Link>
+              </li>
+              <li>
+                <Link href={`/${language}/experience/abrolhos`}>
+                  {getContent(language, [
+                    'experiences',
+                    'abrolhos',
+                    'intro',
+                    'title',
+                  ])}
+                </Link>
+              </li>
+              <li>
+                <Link href={`/${language}/experience/jericoacoara`}>
+                  {getContent(language, [
+                    'experiences',
+                    'jericoacoara',
+                    'intro',
+                    'title',
+                  ])}
+                </Link>
+              </li>
+              <li>
+                <Link href={`/${language}/experience/manaus`}>
+                  {getContent(language, [
+                    'experiences',
+                    'manaus',
+                    'intro',
+                    'title',
+                  ])}
+                </Link>
+              </li>
+              <li>
+                <Link href={`/${language}/experience/salvador`}>
+                  {getContent(language, [
+                    'experiences',
+                    'salvador',
+                    'intro',
+                    'title',
+                  ])}
+                </Link>
+              </li>
+              <li>
+                <Link href={`/${language}/experience/rio-de-janeiro`}>
+                  {getContent(language, [
+                    'experiences',
+                    'rio-de-janeiro',
+                    'intro',
+                    'title',
+                  ])}
+                </Link>
+              </li>
+              <li>
+                <Link href={`/${language}/experience/pantanal`}>
+                  {getContent(language, [
+                    'experiences',
+                    'pantanal',
+                    'intro',
+                    'title',
+                  ])}
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h5>{getContent(language, ['global', 'events'])}</h5>
+            <ul>
+              <li>
+                <Link href={`/${language}/event/carnival`}>
+                  {getContent(language, [
+                    'events',
+                    'carnival',
+                    'intro',
+                    'title',
+                  ])}
+                </Link>
+              </li>
+              <li>
+                <Link href={`/${language}/event/business`}>
+                  {getContent(language, [
+                    'events',
+                    'business',
+                    'intro',
+                    'title',
+                  ])}
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h5>Latin Travel Express</h5>
+            <ul>
+              <li>
+                <Link href={`/${language}/about`}>
+                  {getContent(language, ['global', 'about'])}
+                </Link>
+              </li>
+              <li>
+                <Link href={`/${language}/services`}>
+                  {getContent(language, ['global', 'services'])}
+                </Link>
+              </li>
+              <li>
+                <Link href={`/${language}/disclaimer`}>
+                  {getContent(language, ['global', 'disclaimer'])}
+                </Link>
+              </li>
+              <li>
+                <Link href={`/${language}/data-protection`}>
+                  {getContent(language, ['global', 'data-protection'])}
+                </Link>
+              </li>
+              <li>
+                <Link href={`/${language}/impressum`}>
+                  {getContent(language, ['global', 'impressum'])}
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h5>{getContent(language, ['global', 'contact'])}</h5>
+            <ul>
+              {getParagraphs(language, [
+                'contact',
+                'content',
+                'paragraphs',
+              ]).map((p, _i) => (
+                <li key={_i} dangerouslySetInnerHTML={{ __html: p }} />
+              ))}
+            </ul>
+          </div>
         </nav>
       </Container>
       <style jsx>{`
--- web/components/sections/Header.tsx
+++ web/components/sections/Header.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import { classNames } from '@/helpers/classnameHelper';
 import { useScrollPosition } from '@/hooks/useScrollPosition';
 import Container from '@/components/layout/Container';
+import { getContent } from '@/helpers/contentHelper';
 
 type Props = { language: string };
 
@@ -24,13 +25,21 @@ const Header: NextPage<Props> = ({ language }) => {
     <header id="header" className={classNames([showHeader ? 'show' : 'hide'])}>
       <Container>
         <nav>
-          <Link href={`/${language}`}>Home</Link>
-          <Link href={`/${language}/about`}>About</Link>
-          <Link href={`/${language}/contact`}>Contact</Link>
-          <Link href={`/${language}/data-protection`}>Data Protection</Link>
-          <Link href={`/${language}/disclaimer`}>Disclaimer</Link>
-          <Link href={`/${language}/impressum`}>Impressum</Link>
-          <Link href={`/${language}/services`}>Services</Link>
+          <Link href={`/${language}`}>Logo</Link>
+          <Link href={`/${language}/#experiences`}>
+            {getContent(language, ['global', 'experiences'])}
+          </Link>
+          <Link href={`/${language}/#events`}>
+            {getContent(language, ['global', 'events'])}
+          </Link>
+          <Link href={`/${language}/services`}>
+            {getContent(language, ['global', 'services'])}
+          </Link>
+          <Link href={`/${language}/contact`}>
+            {getContent(language, ['global', 'contact'])}
+          </Link>
+          <Link href={`/de`}>Deutsch</Link>
+          <Link href={`/pt`}>Português</Link>
         </nav>
       </Container>
       <style jsx>{`
