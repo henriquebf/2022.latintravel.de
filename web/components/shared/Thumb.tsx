899699ecae2386871a2eb0c4d0639b7c6470a873
Henrique Ferreira <henriquebf@gmail.com>
2022-01-30 18:30:27 +0100

    show thumbs in home

--- web/components/shared/Thumb.tsx
+++ web/components/shared/Thumb.tsx
@@ -1,17 +1,31 @@
 import type { NextPage } from 'next';
+import Image from 'next/image';
 import Link from 'next/link';
-import { getContent } from '@/helpers/contentHelper';
+import { getContent, getThumb } from '@/helpers/contentHelper';
 
 type Props = { language: string; section: string; thumb: string };
 
 const Thumb: NextPage<Props> = ({ language, section, thumb }) => {
   return (
     <div className="thumb">
-      <Link href={`/${language}/${section}/${thumb}`}>
-        {getContent(language, [section, thumb, 'intro', 'title'])}
+      <Link href={`/${language}/${section}/${thumb}`} passHref>
+        <div>
+          <div className="image">
+            <Image
+              src={getThumb(section, thumb)}
+              alt="image"
+              layout={'fill'}
+              objectFit={'cover'}
+            />
+          </div>
+          {getContent(language, [section, thumb, 'intro', 'title'])}
+        </div>
       </Link>
       <style jsx>{`
-        .thumb {
+        .image {
+          position: relative;
+          width: 100%;
+          height: 200px;
         }
       `}</style>
     </div>
